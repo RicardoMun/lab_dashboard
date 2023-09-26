0d95bdaf1066fcfaa8788861bc907e36034eb3f4
RicardoMun <richy.2603@gmail.com>
2023-09-26 14:24:24 -0500

    Adding authRoutes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
+
 
 /* Server */
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json()); /* translate req.body  */
+
+app.use("/api", authRoutes);
+
 
 export default app;
