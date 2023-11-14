112de479bca3d0b705b5b8c6d25345d88f9ba8b6
Ricardo <richy.2603@gmail.com>
2023-11-14 16:40:14 -0500

    Add validateToken middleware and profile route with authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 /* Enrutador para tener multiples rutas */
 import { Router } from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
-
+import { register, login, logout, profile } from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
+
+
+router.get('/profile', authRequired, profile)
 
 
 export default router;
