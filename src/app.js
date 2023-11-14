ffd9ba7913fcc4cf7f5ddcb56205bc364d61c40e
Ricardo <richy.2603@gmail.com>
2023-11-14 16:39:43 -0500

    Add cookie-parser and tasksRoutes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
-
+import tasksRoutes from "./routes/tasks.routes.js";
+import cookieParser from "cookie-parser";
 
 /* Server */
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json()); /* translate req.body  */
+app.use(cookieParser());
 
 app.use("/api", authRoutes);
+app.use("/api", tasksRoutes);
 
 
 export default app;
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try { 
-    await mongoose.connect("mongodb+srv://RicardoM:uBUnSPuV6f4GbB4d@cluster0.llrzfx3.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://RicardoM:uBUnSPuV6f4GbB4d@LabDB.llrzfx3.mongodb.net/?retryWrites=true&w=majority");
     console.log("Database connected");
   } catch (error) {
     console.log(error);
