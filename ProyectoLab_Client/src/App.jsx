0fb54b206e9d262112a811566dfb172b29bf80df
Ricardo <richy.2603@gmail.com>
2023-11-16 17:50:05 -0500

    Add font and styling to App component

--- ProyectoLab_Client/src/App.jsx
+++ ProyectoLab_Client/src/App.jsx
@@ -1,8 +1,17 @@
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>     
+    <div className="main">
+      
+      <div className="container">
+        
+        <div className="header">
+          <h1>Header</h1>
+        </div>
+
+        <h1>App</h1>     
+      </div>
+      
     </div>
   )
 }
