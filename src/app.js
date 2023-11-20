9854b2eb98c125dd0b055d6bd1431a96b017cdba
Ricardo <richy.2603@gmail.com>
2023-11-20 01:19:53 -0500

    Add CORS configuration for client URL

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 /* Server */
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173", // only client url
+    credentials: true
+}));
 app.use(morgan("dev"));
 app.use(express.json()); /* translate req.body  */
 app.use(cookieParser());
