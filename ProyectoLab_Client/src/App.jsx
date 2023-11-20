67f9fc1bf50a6a9b4ede19153a856b2bd3e14d5c
Ricardo <richy.2603@gmail.com>
2023-11-20 02:23:19 -0500

    Refactor routes and add AuthProvider

--- ProyectoLab_Client/src/App.jsx
+++ ProyectoLab_Client/src/App.jsx
@@ -1,21 +1,26 @@
 import './App.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/Register/RegisterPage';
 import LoginPage from './pages/Login/LoginPage';
+import { AuthProvider } from './context/AuthContext';
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <h1>Home</h1> } />
-        <Route path="/login" element={ <LoginPage/> } />
-        <Route path="/register" element={ <RegisterPage/> } />
-        <Route path="/tasks" element={ <h1>Task page</h1> } />
-        <Route path="/add-task" element={ <h1>Add task</h1> } />
-        <Route path="/tasks/:id" element={ <h1>Update task</h1> } />
-        <Route path="/profile" element={ <h1>Profile</h1> } />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Task page</h1>} />
+          <Route path="/add-task" element={<h1>Add task</h1>} />
+          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
