84fab9672d1457ce1305fb0072f6b4115e65b275
Ricardo <richy.2603@gmail.com>
2023-11-19 19:02:39 -0500

    Update font and routing in App component

--- ProyectoLab_Client/src/App.jsx
+++ ProyectoLab_Client/src/App.jsx
@@ -1,17 +1,24 @@
+import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-export default function App() {
+
+function App() {
   return (
-    <div className="main">
-      
-      <div className="container">
-        
-        <div className="header">
-          <h1>Header</h1>
-        </div>
-
-        <h1>App</h1>     
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <h1>Home</h1> } />
+        <Route path="/login" element={ <h1>Login</h1> } />
+        <Route path="/register" element={ <h1>Register</h1> } />
+        <Route path="/tasks" element={ <h1>Task page</h1> } />
+        <Route path="/add-task" element={ <h1>Add task</h1> } />
+        <Route path="/tasks/:id" element={ <h1>Update task</h1> } />
+        <Route path="/profile" element={ <h1>Profile</h1> } />
+      </Routes>
+      <div className="App">
+        <h1>Lab Client</h1>
       </div>
-      
-    </div>
+    </BrowserRouter>
   )
 }
+
+export default App;
