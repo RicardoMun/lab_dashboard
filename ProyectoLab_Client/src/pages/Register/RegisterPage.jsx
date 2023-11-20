3d9807e38d74b873aade021eedab8a6e2be75a7d
Ricardo <richy.2603@gmail.com>
2023-11-20 02:22:56 -0500

    Add authentication functionality to RegisterPage component

--- ProyectoLab_Client/src/pages/Register/RegisterPage.jsx
+++ ProyectoLab_Client/src/pages/Register/RegisterPage.jsx
@@ -1,17 +1,30 @@
 import "./style.css"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import TermsAndConds from "../../components/TermsAndConditions/TermsAndConds";
-import {registerReqiest} from "../../api/auth.js"
-
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from 'react-router-dom';
 function RegisterPage() {
 
-    const { register, handleSubmit } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const { signup, isAuthenticated, errors: RegisterErrors } = useAuth();
+    const navigate = useNavigate();
 
-    const onSubmit = handleSubmit(async(values) => {
-        const res = await registerReqiest(values)
-        console.log(res);
+    /* redirection when register */
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/tasks")
+        }
+    }, [isAuthenticated])
+
+
+    const onSubmit = handleSubmit(async (values) => {
+        signup(values)
     })
 
     return (
@@ -19,26 +32,48 @@ function RegisterPage() {
         <div className="container">
 
             <div className="form">
+                {
+                    RegisterErrors.map((error, i) => {
+                        return (
+                            <div key={i} className="alertMessage">
+                                {error}
+                            </div>
+                        )
 
+                    })
+                }
+                <h1 className="title">Registro</h1>
                 <form
                     onSubmit={onSubmit}
                 >
-                    <h1 className="title">Registro</h1>
                     <label className="form-label">Usuario</label>
                     <input
                         type="text" {...register("username", { required: true })}
                         className="form-control"
                     />
+                    {errors.username && (
+                        <span className="error">El nombre de usuario es requerido</span>)
+                    }
+
                     <label className="form-label">Correo</label>
                     <input
                         type="text" {...register("email", { required: true })}
                         className="form-control"
                     />
+                    {errors.email && (
+                        <span className="error">El correo es requerido</span>)
+                    }
+
                     <label className="form-label">Contraseña</label>
                     <input
                         type="password" {...register("password", { required: true })}
                         className="form-control"
                     />
+                    {errors.password && (
+                        <span className="error">La contraseña es requerida</span>)
+                    }
+
+
                     <button
                         type="submit"
                         className="sendButton"
@@ -51,7 +86,7 @@ function RegisterPage() {
                             type="checkbox"
                             /* onClick={checkboxHandler} */
                             {...register("terms", { required: true })}
-                            
+
                         />
                         Acepto los {" "}
                         <span className="terms" onClick={() => setIsModalOpen(true)}>
