c7eb957bef7fb95812ee6642032a66dcab9e3cd2
Ricardo <richy.2603@gmail.com>
2023-11-20 01:20:06 -0500

    Update font family in App.css and add Login and Register pages in App.jsx

--- ProyectoLab_Client/src/App.jsx
+++ ProyectoLab_Client/src/App.jsx
@@ -1,22 +1,20 @@
 import './App.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-
+import RegisterPage from './pages/Register/RegisterPage';
+import LoginPage from './pages/Login/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <h1>Home</h1> } />
-        <Route path="/login" element={ <h1>Login</h1> } />
-        <Route path="/register" element={ <h1>Register</h1> } />
+        <Route path="/login" element={ <LoginPage/> } />
+        <Route path="/register" element={ <RegisterPage/> } />
         <Route path="/tasks" element={ <h1>Task page</h1> } />
         <Route path="/add-task" element={ <h1>Add task</h1> } />
         <Route path="/tasks/:id" element={ <h1>Update task</h1> } />
         <Route path="/profile" element={ <h1>Profile</h1> } />
       </Routes>
-      <div className="App">
-        <h1>Lab Client</h1>
-      </div>
     </BrowserRouter>
   )
 }
