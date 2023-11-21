40790ee98d0c79f01b4586023b70f4355cd3c37f
Ricardo <richy.2603@gmail.com>
2023-11-21 14:05:28 -0500

    Update routes and components in App.jsx

--- ProyectoLab_Client/src/App.jsx
+++ ProyectoLab_Client/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import RegisterPage from './pages/Register/RegisterPage';
 import LoginPage from './pages/Login/LoginPage';
-import { AuthProvider } from './context/AuthContext';
+import HomePage from './pages/Home/HomePage';
+import TasksPage from './pages/Tasks/TasksPage';
+import TasksFormPage from './pages/Tasks/TasksFormPage';
+import ProfilePage from './pages/Profile/ProfilePage';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './ProtectedRoute'
 
 
 function App() {
@@ -11,13 +19,17 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Task page</h1>} />
-          <Route path="/add-task" element={<h1>Add task</h1>} />
-          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
-          <Route path="/profile" element={<h1>Profile</h1>} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/add-task" element={<TasksFormPage />} />
+            <Route path="/tasks/:id" element={<TasksFormPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
