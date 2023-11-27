953a369701bf924e617ed914ab3a76a69b438962
Ricardo <richy.2603@gmail.com>
2023-11-27 12:00:40 -0500

    Add AboutUsText component

---
+++ ProyectoLab_Client/src/components/Text/AboutUs.jsx
@@ -0,0 +1,16 @@
+import "./style.css"
+
+
+function AboutUsText() {
+    return (
+        <p className="aboutUsP">En <b>Cristina Vela Laboratorio Clínico Especializado</b>, nos dedicamos a brindar servicios de diagnóstico de vanguardia con un enfoque centrado en la precisión y la atención al paciente.
+        Con años de experiencia en el campo de la salud, nuestro equipo de profesionales altamente calificados trabaja incansablemente para ofrecer resultados de laboratorio confiables y oportunos.
+        Nos enorgullece ser un referente en la industria, destacándonos por la tecnología de última generación que utilizamos y nuestro compromiso inquebrantable con la excelencia en el servicio.
+        Estamos dedicados a proporcionar a médicos y pacientes la información crucial que necesitan para tomar decisiones informadas sobre la salud.
+        En cada análisis y prueba que realizamos, nos esforzamos por superar las expectativas, contribuyendo así al bienestar general de la comunidad a la que servimos.
+    </p>
+    )
+    
+}
+
+export default AboutUsText;
