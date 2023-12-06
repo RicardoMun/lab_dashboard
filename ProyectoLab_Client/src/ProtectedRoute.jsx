af949336b648aeffa94fbc2238dbb5df905a395f
Ricardo <richy.2603@gmail.com>
2023-12-05 19:22:43 -0500

    Refactor ProtectedRoute component

--- ProyectoLab_Client/src/ProtectedRoute.jsx
+++ ProyectoLab_Client/src/ProtectedRoute.jsx
@@ -1,24 +1,10 @@
-import { useAuth } from "./context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 
-
-function ProtectedRoute(){
-
-    const { user, isAuthenticated, loading } = useAuth()
-    
-    if(loading) 
-        return <h1> Loading...</h1>
-
-    if(!loading && !isAuthenticated) 
-        return <Navigate to="/login" replace />
-
-    
-
-    return (
-        <Outlet />
-    )
-
-
-}
-
-export default ProtectedRoute;
+export const ProtectedRoute = () => {
+  const { isAuthenticated, loading } = useAuth();
+  
+  if (loading) return <h1>Loading...</h1>;
+  if (!isAuthenticated && !loading) return <Navigate to="/login" replace />;
+  return <Outlet />;
+};
